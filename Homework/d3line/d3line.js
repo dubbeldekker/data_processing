6b2abd27269ad40213728cb507d74db1973ab08f
dubbeldekker <marije.dekker@hotmail.com>
2016-05-11 15:52:13 +0200

    d3line

---
+++ Homework/d3line/d3line.js
@@ -0,0 +1,97 @@
+// Marije Dekker, 10785949
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%Y-%m-%d").parse,
+    bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.gemTemp/10); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json("d3line.json", function(data){
+		data.forEach(function(d){
+			d.date = parseDate(d.date);
+		  	d.gemTemp = +d.gemTemp;
+		});
+	 
+	x.domain(d3.extent(data, function(d) { return d.date; }));
+  	y.domain(d3.extent(data, function(d) { return d.gemTemp/10; }));
+
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis)
+	  .append("text")
+	  	.attr("text-anchor", "end")
+	  	.attr("dx", "28.5em")
+	  	.attr("dy", "1.9em")
+	  	.text("datum");
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	  .append("text")
+	    .attr("transform", "rotate(90)")
+	    .attr("y", 6)
+	    .attr("dy", "2em")
+	    .attr("dx", "20em")
+	    .style("text-anchor", "end")
+	    .text("temperatuur");
+
+	svg.append("path")
+	    .datum(data)
+	    .attr("class", "line")
+	    .attr("d", line);
+
+	var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+	focus.append("circle")
+	    .attr("r", 4.5);
+
+	focus.append("text")
+      .attr("x", 9)
+      .attr("dy", ".35em");
+
+    svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  function mousemove() {
+    var x0 = x.invert(d3.mouse(this)[0]),
+        i = bisectDate(data, x0, 1),
+        d0 = data[i - 1],
+        d1 = data[i],
+        d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+    focus.attr("transform", "translate(" + x(d.date) + "," + y(d.gemTemp/10) + ")");
+    focus.select("text").text(d.gemTemp/10);
+  }
+});
